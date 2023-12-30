e0092ad97f821508862c60755f98d2a2c73273d0
Tertius Roach <tertius.roach@gmail.com>
2023-12-30 18:59:26 +0200

    README.md

---
+++ src/gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const typescript = require('gulp-typescript');
+
+gulp.task('copyIndex', async () => {
+  let pageName = 'index';
+
+  compileTypescript(pageName);
+});
+
+const compileTypescript = (pageName) => {
+  let fileLocations = `front-end/pages/${pageName}/**/*.ts`;
+  let project = typescript.createProject('../tsconfig.json');
+
+  //--🠋 Compile all Page TypeScript files to JavaScript 🠋--//
+  gulp
+    //--| Description Here |--//
+    .src(`src/${fileLocations}`)
+    //--| Description Here |--//
+    .pipe(project())
+    //--| Compress JavaScript |--//
+    .pipe(uglify())
+    //--| Copy 'src' to 'dist'  |--//
+    .pipe(gulp.dest([`dist/front-end/pages/${pageName}//`]));
+};
